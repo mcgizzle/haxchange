feb20fd930b7e1b001c2832ece179d5712dc11be
Sean McGroarty <mcgroas@tcd.ie>
2018-01-18 17:07:24 +0000

    fixed: getTicker

--- src/Internal.hs
+++ src/Internal.hs
@@ -15,11 +15,8 @@ runApi :: FromJSON r => Opts -> IO (Either String r)
 runApi opts@Opts{..} = do
         let opts' = defaults & header "Accept" .~ ["application/json"] & params .~  optParams
         res <- getWith opts' $ "https://bittrex.com/api/v1.1/public/"++ optPath
-        let success = res ^. responseBody . key "success" . _String
         let msg = res ^. responseBody . key "message" . _String
         let Just p = res ^? responseBody . key "result"  
-        let success' = unpack success
-        if success' == "true" then case fromJSON p of
-                                     Success s -> return $ Right s
-                                     Error e -> return $ Left $ "Error: " ++ e
-                              else return $ Left $ "Error: " ++ unpack msg
+        case fromJSON p of
+          Success s -> return $ Right s
+          Error e -> return $ Left $ "Parse Error: " ++ e
