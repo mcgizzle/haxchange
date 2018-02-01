dd63220e4396ae93b7368ed90156cf8c65619e59
Sean McGroarty <mcgroas@tcd.ie>
2018-02-01 23:05:39 +0000

    added: tests for failure

--- test/Kraken/ApiSpec.hs
+++ test/Kraken/ApiSpec.hs
@@ -9,15 +9,23 @@ import Kraken.Api
 spec :: Spec
 spec = do
         let m = MarketName (COIN BTC) (FIAT EUR)
+        let badM = MarketName (COIN BTC) (COIN BTC)
         describe "GET" $ do
-                it "Gets account balance from Kraken" $ do
+                it "Account balance" $ do
                         res <- getBalance
                         res `shouldSatisfy` isRight
-                it "Gets ticker for BTC/EUR Market" $ do
+                it "Ticker for BTC/EUR market" $ do
                         res <- getTicker m
                         res `shouldSatisfy` isRight
+                it "Ticker for BTC/BTC market" $ do
+                        res <- getTicker badM
+                        res `shouldSatisfy` isLeft
         describe "POST" $ do
-                it "Places (test) buy" $ do
+                it "Buy (correct info)" $ do
                         let o = Order m "100.00" "100.00"
                         res <- buyLimit o
                         res `shouldSatisfy` isRight
+                it "Buy (incorrect info)" $ do
+                        let o = Order badM "" ""
+                        res <- buyLimit o
+                        res `shouldSatisfy` isLeft
