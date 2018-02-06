d810f13405e50bbc255c426ef2502461710eef2a
Sean McGroarty <mcgroas@tcd.ie>
2018-02-06 18:10:07 +0000

    refactored: Internal

--- src/Kraken/Internal.hs
+++ src/Kraken/Internal.hs
@@ -4,14 +4,16 @@
 module Kraken.Internal where
 
 import Debug.Trace
---------------------------------------------------------------------------------------------------
+
 import Types (Opts(..))
 import Utils
 import Kraken.Types
---------------------------------------------------------------------------------------------------
+
 import           Util
 import           Control.Arrow (first)
 import           Network.Wreq
+import           Network.HTTP.Client (HttpException)
+import           Control.Exception as E
 import           Control.Lens 
 import           Data.Aeson.Lens 
 import           Data.Aeson
@@ -22,12 +24,13 @@ import           Data.Text.Encoding (encodeUtf8,decodeUtf8)
 import           Data.List (intercalate)
 import           Data.List.Split (splitOn)
 import           Data.Maybe (fromJust)
+import           Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as Byte
 import           Data.ByteString.Base64 as B64
 import           Crypto.Hash.SHA512 as SHA512 
 import           Crypto.Hash.SHA256 as SHA256
-----------------------------------------------------------------------------------------------------
 
+-- HELPER FUNCTIONS ---------------------------------------------------------------------------
 getUrl :: Opts -> ([String] -> [String]) -> String
 getUrl Opts{..} f = intercalate "/" $ f [ "https://api.kraken.com"
                                         , "0"
@@ -37,29 +40,41 @@ getUrl Opts{..} f = intercalate "/" $ f [ "https://api.kraken.com"
 getUri :: Opts -> String 
 getUri opts = getUrl opts (\ (x:xs) -> "":xs)
 
-----------------------------------------------------------------------------------------------------
+apiSign :: Opts -> String -> ByteString
+apiSign opts@Opts{..} nonce = B64.encode $ SHA512.hmac b64Api 
+                                         $ uri <> nonceAndPost
+       where uri = Byte.pack $ getUri opts
+             nonceAndPost = SHA256.hash $ Byte.pack nonce <> Byte.pack ("nonce="++nonce) 
+             Right b64Api = B64.decode optApiPrivKey
 
+-- HEADERS -----------------------------------------------------------------------------------
+getDefaults :: Opts -> Network.Wreq.Options
+getDefaults Opts{..} = defaults & header "Accept" .~ ["application/json"] 
+                                & params .~  optParams
+
+postDefaults :: String -> Opts -> Network.Wreq.Options
+postDefaults nonce opts@Opts{..} = getDefaults opts & header "API-Key" .~ [optApiPubKey] 
+                                                    & header "API-Sign" .~ [apiSign opts nonce]
+                                                    & header "Content-Type" .~ ["application/x-www-form-urlencoded"]
+
+-- API CALLS ----------------------------------------------------------------------------------
 runGetApi :: FromJSON r => Opts -> Bool -> IO (Either String r)
 runGetApi opts@Opts{..} b = do
-        let opts' = defaults & header "Accept" .~ ["application/json"] 
-                             & params .~  optParams
+        let opts' = getDefaults opts  
             url = getUrl opts id
-        getWith opts' url >>= handleRes b
+        (getWith opts' url >>= handleRes b) `E.catch` handleExcept
 
 runPostApi :: FromJSON r => Opts -> Bool -> IO (Either String r)
 runPostApi opts@Opts{..} b = do
         nonce <- getNonce
-        let body = [ "nonce" := nonce ] <> toFormParam optPost 
+        let opts' = postDefaults nonce opts 
             url = getUrl opts id
-            apisign = B64.encode $ SHA512.hmac b64Api (uri <> nonceAndPost)
-            uri = Byte.pack $ getUri opts
-            nonceAndPost = SHA256.hash $ Byte.pack nonce <> Byte.pack ("nonce="++nonce) 
-            Right b64Api = B64.decode optApiPrivKey
-            opts' = defaults & header "API-Key" .~ [optApiPubKey] 
-                             & header "API-Sign" .~ [apisign]
-                             & header "Content-Type" .~ ["application/x-www-form-urlencoded"]
-                             & header "Accept" .~ ["application/json"] 
-        postWith opts' url body >>= handleRes b
+            body = [ "nonce" := nonce ] <> toFormParam optPost 
+        (postWith opts' url body >>= handleRes b) `E.catch` handleExcept
+
+-- HANDLER ----------------------------------------------------------------------------------
+handleExcept :: FromJSON r => HttpException -> IO (Either String r)
+handleExcept e = return $ Left $ "Network Exception: " ++ show e
 
 handleRes :: (Show a, FromJSON b, AsValue a) => Bool -> Response a -> IO (Either String b)
 handleRes member res = do
